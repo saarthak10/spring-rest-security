ab61e69e98d4ba5b63f2abca7238bd30ab34622b
saarthak10 <saarthaksharma87@gmail.com>
2023-05-16 02:15:33 +0530

    basic auth issue solved,database setup for handling users from database

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -12,36 +12,34 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class DemoSecurityConfig {
 
+    @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails saarthak = User.builder()
                 .username("saarthak")
-                .password("test123")
+                .password("{noop}test123")
                 .roles("EMPLOYEE")
                 .build();
-
         UserDetails jack = User.builder()
                 .username("jack")
-                .password("test123")
+                .password("{noop}test1234")
                 .roles("EMPLOYEE", "MANAGER")
                 .build();
-
         UserDetails john = User.builder()
                 .username("john")
-                .password("test123")
+                .password("{noop}test1235")
                 .roles("EMPLOYEE", "MANAGER", "ADMIN")
                 .build();
-
         return new InMemoryUserDetailsManager(saarthak, jack, john);
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(configurer ->
-                configurer.requestMatchers(HttpMethod.GET, "api/employees").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "api/employees/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.POST, "api/employees/**").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "api/employees").hasRole("ADMIN")
+                configurer.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.POST, "/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees").hasRole("ADMIN")
 
         );
 
